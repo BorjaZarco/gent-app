c04d9a895fc6c53a24595f9f2d38b6c824bb788d
Borja Zarco <borja@squaads.com>
2021-06-30 21:13:02 +0100

    add test features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeopleService } from './services/people.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'gent';
+export class AppComponent implements OnInit {
+  people: any[] = [];
+
+  selectedPerson = null;
+
+  constructor(private peopleService: PeopleService) {}
+
+  ngOnInit() {
+    this.people = this.peopleService.getPeople();
+  }
+
+  onSelectPerson(selectedPerson: any) {
+    if (this.selectedPerson?.id === selectedPerson?.id) {
+      this.selectedPerson = null;
+    } else {
+      this.selectedPerson = selectedPerson;
+    }
+    console.log(this.selectedPerson);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonCardComponent } from './components/person-card/person-card.component';
+import { PersonDetailsComponent } from './components/person-details/person-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PersonCardComponent, PersonDetailsComponent],
+  imports: [BrowserModule, AppRoutingModule, AutoCompleteModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
