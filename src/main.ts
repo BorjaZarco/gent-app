26979f47ef5de60e34f8b3a3a8114b24e896640f
Borja Zarco <borja@squaads.com>
2021-07-08 20:37:26 +0100

    set moment locale

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import AOS from 'aos';
+import * as moment from 'moment';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 AOS.init();
+moment.locale('es');
 if (environment.production) {
   enableProdMode();
 }
