5fcf0d7bb5c856168a36d2e54756ad65c75dab06
Borja Zarco <borja@squaads.com>
2021-07-10 09:49:22 +0100

    add titlecasepipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TitleCasePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -41,7 +42,7 @@ import { ObjectPipe } from './pipes/object.pipe';
     RippleModule,
     DropdownModule,
   ],
-  providers: [ObjectPipe],
+  providers: [ObjectPipe, TitleCasePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
